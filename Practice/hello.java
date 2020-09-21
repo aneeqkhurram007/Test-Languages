6123369ba12810f2e86e62e5a85c7de22ed30c7c
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-09-21 16:32:32 +0500

    4.23

--- Practice/hello.java
+++ Practice/hello.java
@@ -1,8 +1,11 @@
+
+
+/**
+ * hello
+ */
 public class hello {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello World");
-
+        System.out.println("Hello");
     }
 }
