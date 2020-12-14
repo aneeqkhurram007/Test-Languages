6f438b3b572df6f7315308d7717fc39e1660bd88
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-12-15 00:36:40 +0500

    12.36

---
+++ React/firebase_crud/src/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyATcbGkoY9KkUubdKBT7WotKLvrMDcNYyQ",
+  authDomain: "react-crud-2d8de.firebaseapp.com",
+  databaseURL: "https://react-crud-2d8de-default-rtdb.firebaseio.com",
+  projectId: "react-crud-2d8de",
+  storageBucket: "react-crud-2d8de.appspot.com",
+  messagingSenderId: "822651366821",
+  appId: "1:822651366821:web:d1c9c3c437db3e63754c4c",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
