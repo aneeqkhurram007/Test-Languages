43d9c49758cd168831700a81e20e32dcb22c9e3c
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-12-14 19:19:34 +0500

    7.19

--- Huraira/Semester 2/Assignment 3/task4/main.cpp
+++ Huraira/Semester 2/Assignment 3/task4/main.cpp
@@ -4,32 +4,41 @@ using namespace std;
 int main()
 {
     fstream file, file1;
+
     file.open("input.txt");
     file1.open("output.txt");
+
     for (int i = 0; !file.eof(); i++)
     {
         char arr[10];
+        char *ptr = arr;
+
         int count = 0;
-        file.getline(arr, 10);
-        for (int i = 0; arr[i] != 0; i++)
+
+        file.getline(ptr, 10);
+
+        for (int i = 0; *(ptr + i) != '\0'; i++)
         {
-            file1 << arr[i];
-            if (arr[i] < 97)
+            file1 << *(ptr + i);
+            if (*(ptr + i) < 97)
             {
-                arr[i] += 32;
+                *(ptr + i) += 32;
             }
             count++;
         }
+
         int flag;
+
         for (int i = 0; i < count; i++)
         {
             flag = 0;
-            if (arr[i] != arr[count - i - 1])
+            if (*(ptr + i) != *(ptr + (count - i - 1)))
             {
                 flag = 1;
                 break;
             }
         }
+
         if (flag == 1)
         {
             file1 << " Not Palindrome";
