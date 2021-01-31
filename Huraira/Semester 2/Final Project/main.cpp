6ac0bc1168baf643a509c651fd085118c40f8da8
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-01-31 13:21:55 +0500

    1.21

---
+++ Huraira/Semester 2/Final Project/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    fstream file;
+    file.open("data.txt");
+    int prodNum;
+    char prodName[50];
+    float retails;
+    int quant;
+    float amount;
+}
