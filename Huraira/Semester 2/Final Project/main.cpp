a52918aa11a3a11d1446cb3b451a2c582f390cfb
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-01-31 21:13:28 +0500

    9.13

--- Huraira/Semester 2/Final Project/main.cpp
+++ Huraira/Semester 2/Final Project/main.cpp
@@ -6,9 +6,32 @@ int main()
 {
     fstream file;
     file.open("data.txt");
-    int prodNum;
-    char prodName[50];
-    float retails;
-    int quant;
-    float amount;
+    int **table = new int *[4];
+    int size = 4;
+    table[0] = new int[size]{101, 102, 103, 104}; //prodNum
+    char **prodName = new char *[size];           //prodName
+    {
+        prodName[0] = new char[50];
+        prodName[1] = new char[50];
+        prodName[2] = new char[50];
+        prodName[3] = new char[50];
+    }
+    table[1] = new int[size]{60, 200, 150, 350}; //retails
+    table[2] = new int[size]{10, 20, 30, 40};    //quant
+    table[4] = new int[size];                    //amount
+
+    for (int i = 0; i < 4; i++)
+    {
+        // for (int j = 0; j < 4; j++)
+        // {
+        //     /* code */
+        // }
+        // gets(prodName[i]);
+        // cout << prodName[i] << " ";
+        for (int j = 0; j < 4; j++)
+        {
+            cout << table[j][i] << " ";
+        }
+        cout << endl;
+    }
 }
