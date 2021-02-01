0b5dfb9b75b3c02644643507ba336a9903b3b86c
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-02-02 01:32:34 +0500

    1.32

--- Huraira/Semester 2/Final Project/regrow.cpp
+++ Huraira/Semester 2/Final Project/regrow.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 using namespace std;
-int *regrow(int *table, int *size)
+int **regrow(int **table, int *size)
 {
     cout << "Old Array: " << endl;
 
     for (int i = 0; i < *size; i++)
     {
-        cout << *(table + i) << " ";
+        for (int j = 0; table[i][j] != 0; j++)
+        {
+            cout << table[i][j] << " ";
+        }
+        cout << endl;
     }
 
     int oldSize = *size;
 
     *size = oldSize * 2;
 
-    int *temp = new int[*size]{0};
+    int **temp = new int *[*size];
     for (int i = 0; i < oldSize; i++)
     {
-        *(temp + i) = *(table + i);
+        temp[i] = table[i];
+        for (int j = 0; table[i][j] != 0; j++)
+        {
+            temp[i][j] = table[i][j];
+        }
     }
     delete[] table;
     table = NULL;
@@ -42,23 +50,48 @@ int *del(int *table, int *size, int element)
 
 int main()
 {
-    int size = 4;
-    int *table = new int[size]{1, 2, 3, 4};
+    // int size = 4;
+    // int *table = new int[size]{1, 2, 3, 4};
 
-    table = regrow(table, &size);
-    table[5] = 5;
-    cout << "\nNew Array" << endl;
-    for (int i = 0; i < size; i++)
+    // table = regrow(table, &size);
+    // table[5] = 5;
+    // cout << "\nNew Array" << endl;
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout << table[i] << " ";
+    // }
+    // cout << "\nSize is: " << size << endl;
+    // table = del(table, &size, 1);
+    // cout << "\nSize is: " << size << endl;
+
+    // cout << "\nNew Array" << endl;
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout << table[i] << " ";
+    // }
+    int matSize = 3;
+    int **mat = new int *[matSize];
+    for (int i = 0; i < matSize; i++)
     {
-        cout << table[i] << " ";
+        int colSize = i + 1;
+        mat[i] = new int[colSize]{0};
+        for (int j = 0; j < colSize; j++)
+        {
+            mat[i][j] = i + j;
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
     }
-    cout << "\nSize is: " << size << endl;
-    table = del(table, &size, 1);
-    cout << "\nSize is: " << size << endl;
-
+    mat = regrow(mat, &matSize);
+    mat[4] = new int[1]{7};
     cout << "\nNew Array" << endl;
-    for (int i = 0; i < size; i++)
+
+    for (int i = 0; i < matSize; i++)
     {
-        cout << table[i] << " ";
+        for (int j = 0; mat[i][j] != 0; j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
     }
 }
