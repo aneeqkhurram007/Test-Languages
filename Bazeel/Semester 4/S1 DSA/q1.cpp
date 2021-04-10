b1a58f32d851b0390276f36ae2dc91080aefe008
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-04-10 14:23:24 +0500

    changing

--- Bazeel/Semester 4/S1 DSA/q1.cpp
+++ Bazeel/Semester 4/S1 DSA/q1.cpp
@@ -1,38 +1,86 @@
 #include <iostream>
 using namespace std;
+int SIZE = 50;
+int top = -1;
+
+char *pop(char *stack, char element)
+{
+    top--;
+    char *temp = new char[SIZE];
+    for (int i = 0, j = 0; i < top; i++)
+    {
+        if (stack[i] != element)
+        {
+            temp[j] = stack[i];
+            j++;
+        }
+    }
+    delete[] stack;
+    return temp;
+}
+char *push(char *stack, char element)
+{
+    top++;
+    int i;
+    char *temp = new char[SIZE];
+    for (i = 0; i < top - 1; i++)
+    {
+
+        temp[i] = stack[i];
+    }
+    temp[top - 1] = element;
+    delete[] stack;
+    return temp;
+}
+bool isEmpty()
+{
+    return top == -1;
+}
+bool isFull()
+{
+    return top == SIZE;
+}
+
 int main()
 {
-
     int top = -1;
     char *stack = new char(50);
-    cout << "Enter a name: ";
-    cin >> stack;
-    int count = 0;
-    for (int i = 0; stack[i] != '\0'; i++)
+    char option;
+    do
     {
-        count++;
-    }
-    int flag = 0;
-    for (int i = 0; i < count; i++)
-    {
-        if (stack[i] == stack[count - i - 1])
+        cout << "Enter a name: ";
+        cin >> stack;
+        int count = 0;
+        for (int i = 0; stack[i] != '\0'; i++)
         {
-            flag = 1;
+            count++;
+        }
+        int flag = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (stack[i] == stack[count - i - 1])
+            {
+                flag = 1;
+            }
+            else
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 1)
+        {
+            cout << stack << " is a palindrome" << endl;
         }
         else
         {
-            flag = 0;
-            break;
+            cout << stack << " is not a palindrome" << endl;
         }
-    }
-    if (flag == 1)
-    {
-        cout << stack << " is a palindrome" << endl;
-    }
-    else
-    {
-        cout << stack << " is not a palindrome" << endl;
-    }
+        cout << "Do you want to continue: ";
+        cin >> option;
+
+    } while (option == 'y');
+    cout << "Thanks for choosing our emulator: " << endl;
 
     return 0;
 }
