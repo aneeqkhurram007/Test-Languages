53a62bd61513f57b146d81a5d4eaa4eb88db950b
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-06-21 12:05:32 +0500

    12.05

--- Saad/Terminal/Person/src/App.java
+++ Saad/Terminal/Person/src/App.java
@@ -8,32 +8,29 @@ public class App {
         ArrayList<Student> students = new ArrayList<Student>();
         ArrayList<Staff> staff = new ArrayList<Staff>();
         String filename = "student.ser";
-        int choice;
-        int restart;
-        do {
-            System.out.println("Menu");
-            System.out.println("Press 1 to enter data for Student.");
-            System.out.println("Press 2 to enter data for Staff.");
-            System.out.println("Enter your choice: ");
-            choice = input.nextInt();
-            int i = 0;
-            int j = 0;
-            switch (choice) {
-                case 1:
-                    students.add(i, getStudent());
-                    i++;
-                    break;
-                case 2:
-                    staff.add(j, getStaff());
-                    j++;
-                    break;
-                default:
-                    break;
-            }
-            System.out.println("Want to start again (1/0): ");
 
-            restart = input.nextInt();
-        } while (restart == 1);
+        System.out.println("Menu");
+        System.out.println("Press 1 to enter data for Student.");
+        System.out.println("Press 2 to enter data for Staff.");
+        System.out.println("Output");
+
+        students.add(new Student("John", "Address1", "BCS", 2021, 1500));
+        students.add(new Student("Alex", "Address2", "BCS", 2021, 1500));
+        students.add(new Student("Adam", "Address3", "BCS", 2021, 1500));
+        students.add(new Student("Trevor", "Address4", "BCS", 2021, 1500));
+        students.add(new Student("Noah", "Address5", "BCS", 2021, 1500));
+
+        staff.add(new Staff("Dave", "Address2", "KSS", 200));
+        staff.add(new Staff("David", "Address2", "KSS", 200));
+        staff.add(new Staff("Jonah", "Address2", "KSS", 200));
+        staff.add(new Staff("Miley", "Address2", "KSS", 200));
+        staff.add(new Staff("Cyrus", "Address2", "KSS", 200));
+        for (Student std : students) {
+            System.out.println(std);
+        }
+        for (Staff staff2 : staff) {
+            System.out.println(staff2);
+        }
         input.close();
         String filename1 = "Staff.ser";
         try {
@@ -58,7 +55,7 @@ public class App {
         }
 
         catch (Exception ex) {
-            System.out.println("Exception is caught");
+
         }
 
         Student studOut;
@@ -90,62 +87,9 @@ public class App {
         }
 
         catch (Exception ex) {
-            System.out.println("Exception is caught");
+
         }
 
-    }
-
-    static Student getStudent() {
-        Scanner input = new Scanner(System.in);
-        Student newStud;
-        String name;
-        String address;
-        String program;
-        int year;
-        double fee;
-        System.out.println("Enter student name: ");
-        do {
-            name = input.nextLine();
-
-        } while (!name.matches("[A-Za-z]*"));
-        System.out.println("Enter address: ");
-        address = input.nextLine();
-        System.out.println("Enter program: ");
-        program = input.nextLine();
-        System.out.println("Enter fee: ");
-        fee = input.nextInt();
-        System.out.println("Enter year: ");
-        year = input.nextInt();
-
-        newStud = new Student(name, address, program, year, fee);
-
-        input.close();
-        return newStud;
-    }
-
-    static Staff getStaff() {
-        Scanner input = new Scanner(System.in);
-        Staff newStaff;
-        String name;
-        String address;
-        String school;
-        double pay;
-        System.out.println("Enter student name: ");
-        do {
-            name = input.nextLine();
-
-        } while (!name.matches("[A-Za-z]*"));
-        System.out.println("Enter address: ");
-        address = input.nextLine();
-        System.out.println("Enter school: ");
-        school = input.nextLine();
-        System.out.println("Enter fee: ");
-        pay = input.nextInt();
-
-        newStaff = new Staff(name, address, school, pay);
-
-        input.close();
-        return newStaff;
     }
 
 }
