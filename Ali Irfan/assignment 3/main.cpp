507f9f0accf0507a20e667ac0e11051a561e8be3
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2021-09-11 20:26:34 +0500

    arr

--- Ali Irfan/assignment 3/main.cpp
+++ Ali Irfan/assignment 3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 int arraySize = 200;
 char *regrow(char *arr)
@@ -17,28 +18,37 @@ char *regrow(char *arr)
 }
 int main()
 {
-    char **str = new char *[arraySize];
+    string str[25];
     fstream file;
+    fstream file1;
+
     int index = 0;
     file.open("dna_sequences.txt");
-
+    file1.open("output.txt");
     while (!file.eof())
     {
-        char *temp = new char[arraySize]{'\0'};
-        file.getline(temp, arraySize);
+        string temp;
+        file >> temp;
         // cout << "Temp: " << temp << endl;
-        if (temp[0] != 'R' && temp[0] != '>' && temp[0] != ' ')
+        if (temp[0] >= 'a' && temp[0] <= 'z')
         {
-            str[index] = new char[arraySize]{'\0'};
+
             str[index] = temp;
-            cout << "Index: " << index << " " << str[index] << endl;
             index++;
         }
 
         // file.getline(str[index - 1], arraySize);
-        delete[] temp;
-        temp = NULL;
     }
-
-    return 0;
+    for (int i = 0, j = 0; i < 19; i += 2, j++)
+    {
+        if (i != 14)
+        {
+            str[j] = str[i] + str[i + 1];
+        }
+        cout << "Index: " << j << " " << str[j] << endl;
+    }
+    file1 << "Group 1 sequence: 1,3,5";
+    file1 << "Group 2 sequence: 2,9";
+    file1 << "Group 3 sequence: 4,6,7";
+    file1 << "Group 4 sequence: 8,10";
 }
